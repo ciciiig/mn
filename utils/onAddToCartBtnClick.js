0fee8d79db097ec81debf36ddc4f7f6017b4d6e3
George <tsintsadze7@gmail.com>
2024-05-15 00:30:26 +0400

    feat: add to cart still works after deletion

--- utils/onAddToCartBtnClick.js
+++ utils/onAddToCartBtnClick.js
@@ -1,16 +1,16 @@
 import { createCart } from "./createCart.js";
 
-const productsInCart = JSON.parse(localStorage.getItem('productsInCart')) || [];
-
-export function onAddToCartBtnClick (event, products, cartBody) {
+export function onAddToCartBtnClick(event, products, cartBody) {
     if (event.target.id && event.target.id.includes('add-to-cart-btn')) {
         const productId = event.target.closest('.card').id;
 
+        let productsInCart = JSON.parse(localStorage.getItem('productsInCart')) || [];
+        
         if (!productsInCart.includes(productId)) {
-            productsInCart.push(productId)
-            localStorage.setItem('productsInCart', JSON.stringify(productsInCart))
+            productsInCart.push(productId);
+            localStorage.setItem('productsInCart', JSON.stringify(productsInCart));
         }
     }
 
-    createCart(products, cartBody)
+    createCart(products, cartBody);
 }
